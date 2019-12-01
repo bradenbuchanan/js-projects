83c4de99e05aa836b2b2b9151353f48982ddf145
Braden Buchanan <bradenbuchanan@ymail.com>
2019-11-30 20:46:42 -0500

    Create app.js

---
+++ numberGuesser/app.js
@@ -0,0 +1,9 @@
+/*
+Game function:
+- player must guess a number between a min and max
+- player gets a certain amount of guesses
+- notify player of guesses remaining
+- notify player of the correct answer if loose
+- let player choose to play again
+*/
+
