d08a2c360825a2b86121e0f0bcc9598a343d637f
Braden Buchanan <bradenbuchanan@ymail.com>
2019-12-03 16:22:44 -0500

    create app.js
    Adding book to list functionality

---
+++ bookList/app.js
@@ -0,0 +1,56 @@
+// Book Constructor
+function Book(title, author, isbn){
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI(){}
+
+// Add book
+UI.prototype.addBookToList = function(book){
+  const list = document.getElementById('book-list');
+
+  // Create tr element
+  const row = document.createElement('tr');
+
+  // Insert col
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+
+  list.appendChild(row);
+}
+
+// Clear fields
+UI.prototype.clearFields = function(){
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+  // Get form values
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author'). value,
+        isbn = document.getElementById('isbn').value
+  
+  // Instaniate book
+  const book = new Book(title, author, isbn);
+
+  // Instaniate UI
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  // Clear fields
+  ui.clearFields()
+
+  e.preventDefault();
+})
