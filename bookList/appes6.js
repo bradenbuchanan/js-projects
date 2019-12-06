05648d5f34078b20aac06d378be6a5c65c64c9dc
Braden Buchanan <bradenbuchanan@ymail.com>
2019-12-05 22:04:51 -0500

    Update appes6.js

--- bookList/appes6.js
+++ bookList/appes6.js
@@ -1,5 +1,4 @@
 // Converting previous app.js into es6 naming conventions
-
 class Book {
   constructor(title, author, isbn) {
     this.title = title;
@@ -24,4 +23,22 @@ class UI {
     list.appendChild(row);
   }
 
-  
+  showAlert(message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.container');
+    // Get form
+    const form = document.querySelector('#book-form');
+    // Insert alert
+    container.insertBefore(div, form);
+
+    // Timeout after 3 sec
+    setTimeout(function(){
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
